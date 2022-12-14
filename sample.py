68063b8db2ac459bc12eb39f0001d3ef7174f95c
studybhanu <113280481+studybhanu@users.noreply.github.com>
2022-12-14 09:55:53 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,7 @@
+list_1 = [10,20,30,40,50]
+res_list1 = [x*2 for i in list1]
+print(res_list1)
+list_2 = [100,200,300,400,500]
+res_list2 = [x*0.2 for i in list_2]
+print(res_list2)
+print("list completed")
